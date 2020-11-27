c2a1e9a4b101b47958a2cee0421dcdbf7433c6a5
Alex Sbille <as@fasteo.com>
2020-11-28 00:55:05 +0100

    Maksiu1000 made this feature

--- maps/biter_battles_v2/ai.lua
+++ maps/biter_battles_v2/ai.lua
@@ -417,6 +417,7 @@ Public.wake_up_sleepy_groups = function()
 	end
 end
 
+--By Maksiu1000 skip the last two tech 
 Public.unlock_satelite = function()
     if game.forces.north.technologies['speed-module-3'].researched 
     and not game.forces.north.technologies['rocket-silo'].researched == true then
