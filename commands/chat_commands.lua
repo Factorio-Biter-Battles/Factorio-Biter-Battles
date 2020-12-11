4bcd9c62af3da80974bdc6dcbcfc10df792e7aac
mysticamber <gamingmysticamber@gmail.com>
2020-12-10 22:19:40 -0500

    biter-battles: adding north-chat and south-chat commands

--- commands/chat_commands.lua
+++ commands/chat_commands.lua
@@ -16,36 +16,36 @@ local function chat_with_team(message, team)
 
     local msg = "[To " .. team .. "] " .. player_name .. tag .. " (" ..
                     player.force.name .. "): " .. message
+
     game.forces.spectator.print(msg, color)
-    if (team == "North") then
+
+    if (team == "north" or player.force.name == "north") then
         game.forces.north.print(msg, color)
-    else
+    end
+    if (team == "south" or player.force.name == "south" ) then
         game.forces.south.print(msg, color)
     end
-
     Server.to_discord_player_chat(msg)
 end
 
 commands.add_command('sth', 'chat with south', function(cmd)
     local message = tostring(cmd.parameter)
-   chat_with_team(message,'South')
+   chat_with_team(message,'south')
 end)
 
 commands.add_command('south-chat', 'Chat with south. You can also use /sth', function(cmd)
     game.player.print("System: You can also you /sth")
     local message = tostring(cmd.parameter)
-   chat_with_team(message,'South')
+   chat_with_team(message,'south')
 end)
-
-
 
 commands.add_command('nth', 'Chat with north. Same as /north-chat', function(cmd)
     local message = tostring(cmd.parameter)
-   chat_with_team(message,'North')
+   chat_with_team(message,'north')
 end)
 
 commands.add_command('north-chat', 'Chat with north. You can also use /nth', function(cmd)
     game.player.print("System: You can also you /nth")
     local message = tostring(cmd.parameter)
-   chat_with_team(message,'North')
+   chat_with_team(message,'north')
 end)
