971fe762f0c97bb1f9bc18dddc94ff2438268754
guymaor86 <guymaor86@gmail.com>
2021-02-26 10:14:54 -0700

    Revert "Improve biter pathfinding"

--- maps/biter_battles_v2/ai.lua
+++ maps/biter_battles_v2/ai.lua
@@ -9,12 +9,14 @@ local vector_radius = 512
 local attack_vectors = {}
 attack_vectors.north = {}
 attack_vectors.south = {}
-for p = 0.3, 0.71, 0.1 do
-	local a = math.pi * p
-	local x = vector_radius * math.cos(a)
-	local y = vector_radius * math.sin(a)
-	attack_vectors.north[#attack_vectors.north + 1] = {x, y * -1}
-	attack_vectors.south[#attack_vectors.south + 1] = {x, y}
+for x = vector_radius * -1, vector_radius, 1 do
+	for y = 0, vector_radius, 1 do
+		local r = math.sqrt(x ^ 2 + y ^ 2)
+		if r < vector_radius and r > vector_radius - 1 then
+			attack_vectors.north[#attack_vectors.north + 1] = {x, y * -1}
+			attack_vectors.south[#attack_vectors.south + 1] = {x, y}
+		end
+	end
 end
 local size_of_vectors = #attack_vectors.north
 
@@ -228,24 +230,6 @@ local function select_units_around_spawner(spawner, force_name, side_target)
 	return valid_biters
 end
 
-local group_path_flags_nocache_straight =
-{
-  cache = false,
-  prefer_straight_paths = true
-}
-
-local group_path_flags_cache_straight_lowprio =
-{
-  prefer_straight_paths = true,
-  low_priority = true
-}
-
-local group_path_flags_nocache_nobreak =
-{
-  cache = false,
-  no_break = true
-}
-
 local function send_group(unit_group, force_name, side_target)
 	local target
 	if side_target then
@@ -259,49 +243,32 @@ local function send_group(unit_group, force_name, side_target)
 	
 	local commands = {}	
 	local vector = attack_vectors[force_name][math_random(1, size_of_vectors)]
-	local distance_modifier = math_random(1, 8) * 0.128
+	local distance_modifier = math_random(25, 100) * 0.01
 	
 	local position = {target.x + (vector[1] * distance_modifier), target.y + (vector[2] * distance_modifier)}
 	position = unit_group.surface.find_non_colliding_position("stone-furnace", position, 96, 1)
 	if position then
 		if math.abs(position.y) < math.abs(unit_group.position.y) then
 			commands[#commands + 1] = {
-				type = defines.command.go_to_location,
+				type = defines.command.attack_area,
 				destination = position,
-				radius = 64,
-				distraction = defines.distraction.by_enemy,
-				pathfind_flags = group_path_flags_cache_straight_lowprio
+				radius = 16,
+				distraction = defines.distraction.by_enemy
 			}
 		end
 	end
 	
-    commands[#commands + 1] = {
-		type = defines.command.go_to_location,
-		destination = target,
-		radius = 64,
-		distraction = defines.distraction.by_enemy,
-		pathfind_flags = group_path_flags_nocache_straight
-	}
-	
 	commands[#commands + 1] = {
 		type = defines.command.attack_area,
 		destination = target,
-		radius = 16,
-		distraction = defines.distraction.by_enemy,
-	}
-	
-    commands[#commands + 1] = {
-		type = defines.command.go_to_location,
-		destination = global.rocket_silo[force_name].position,
-        radius = 64,
-		distraction = defines.distraction.by_damage,
-		pathfind_flags = group_path_flags_nocache_nobreak
+		radius = 32,
+		distraction = defines.distraction.by_enemy
 	}
 	
 	commands[#commands + 1] = {
 		type = defines.command.attack,
 		target = global.rocket_silo[force_name],
-		distraction = defines.distraction.by_damage,
+		distraction = defines.distraction.by_enemy
 	}
 	
 	unit_group.set_command({
