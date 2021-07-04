21c5692b03cd49d7639ac99674eb1dd8e8297b23
Aleksander Mistewicz <a.mistewicz@gmail.com>
2021-07-04 20:15:49 +0200

    ai: Lower max_group_size to 300
    Large groups, mean more biters at one place affected by poison
    capsules/flamers and other AOE damage. Maybe it improves performance.

--- maps/biter_battles_v2/config.lua
+++ maps/biter_battles_v2/config.lua
@@ -11,7 +11,7 @@ local bb_config = {
 	["random_scrap"] = true,							--Generate harvestable scrap around worms randomly?
 
 	--BITER SETTINGS--
-	["max_group_size"] = 420,							--Maximum unit group size.
+	["max_group_size"] = 300,							--Maximum unit group size.
 	["biter_timeout"] = 162000,						--Time it takes in ticks for an attacking unit to be deleted. This prevents permanent stuck units.
 	["bitera_area_distance"] = 512					--Distance to the biter area.
 }
