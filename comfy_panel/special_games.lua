ff6658c915f304df7616636ce71459723c8d0938
TBZ <92057893+TheBigZet@users.noreply.github.com>
2021-11-26 15:50:28 +0100

    Added description of the mode in tooltip Added "operable" config for inf. chest Compressed inf, chest config to fit more items

--- comfy_panel/special_games.lua
+++ comfy_panel/special_games.lua
@@ -5,7 +5,7 @@ local valid_special_games = {
 	Add your special game here.
 	Syntax:
 	<game_name> = {
-		name = "<Name displayed in gui>"
+		name = {type = "label", caption = "<Name displayed in gui>", tooltip = "<Short description of the mode"
 		config = {
 			list of all knobs, leavers and dials used to config your game
 			[1] = {name = "<name of this element>" called in on_gui_click to set variables, type = "<type of this element>", any other parameters needed to define this element},
@@ -17,7 +17,7 @@ local valid_special_games = {
 	}
 	]]
 	turtle = {
-		name = "Turtle",
+		name = {type = "label", caption = "Turtle", tooltip = "Generate moat with given dimensions around the spawn"},
 		config = {
 			[1] = {name = "label1", type = "label", caption = "moat width"},
 			[2] = {name = 'moat_width', type = "textfield", text = "5", numeric = true, width = 40},
@@ -33,16 +33,19 @@ local valid_special_games = {
 	},
 
 	infinity_chest = {
-		name = "Infinity chest",
+		name = {type = "label", caption = "Infinity chest", tooltip = "Spawn infinity chests with given filters"},
 		config = {
-			[1] = {name = "separate_chests", type = "switch", left_label_caption = "Single", right_label_caption = "Multi",	switch_state = "left"},
-			[2] = {name = "label1", type = "label", caption = "Gap size"},
-			[3] = {name = "gap", type = "textfield", text = "3", numeric = true, width = 40},
-			[4] = {name = "eq1", type = "choose-elem-button", elem_type = "item"},
-			[5] = {name = "eq2", type = "choose-elem-button", elem_type = "item"},
-			[6] = {name = "eq3", type = "choose-elem-button", elem_type = "item"},
-			[7] = {name = "eq4", type = "choose-elem-button", elem_type = "item"},
-			[8] = {name = "eq5", type = "choose-elem-button", elem_type = "item"}			
+			[1] = {name = "separate_chests", type = "switch", switch_state = "left", tooltip = "Single chest / Multiple chests"},
+			[2] = {name = "operable", type = "switch", switch_state = "left", tooltip = "Operable? Y / N"},
+			[3] = {name = "label1", type = "label", caption = "Gap size"},
+			[4] = {name = "gap", type = "textfield", text = "3", numeric = true, width = 40},
+			[5] = {name = "eq1", type = "choose-elem-button", elem_type = "item"},
+			[6] = {name = "eq2", type = "choose-elem-button", elem_type = "item"},
+			[7] = {name = "eq3", type = "choose-elem-button", elem_type = "item"},
+			[8] = {name = "eq4", type = "choose-elem-button", elem_type = "item"},
+			[9] = {name = "eq5", type = "choose-elem-button", elem_type = "item"},
+			[10] = {name = "eq6", type = "choose-elem-button", elem_type = "item"},
+			[11] = {name = "eq7", type = "choose-elem-button", elem_type = "item"}
 		},
 		button = {name = "infinity_chest_apply", type = "button", caption = "Apply"}
 	}
@@ -55,7 +58,7 @@ local function generate_turtle(moat_width, entrance_width, size_x, size_y)
 	local water_positions = {}
 	local concrete_positions = {}
 	local landfill_positions = {}
-	
+
 	for i = 0, size_y + moat_width do -- veritcal canals
 		for a = 1, moat_width do
 			table.insert(water_positions, {name = "deepwater", position = {x = (size_x / 2) + a, y = i}})
@@ -73,8 +76,10 @@ local function generate_turtle(moat_width, entrance_width, size_x, size_y)
 
 	for i = 0, entrance_width - 1 do
 		for a = 1, moat_width + 6 do
-			table.insert(concrete_positions, {name = "refined-concrete", position = {x = -entrance_width / 2 + i, y = size_y - 3 + a}})
-			table.insert(concrete_positions, {name = "refined-concrete", position = {x = -entrance_width / 2 + i, y = -size_y + 2 - a}})
+			table.insert(concrete_positions,
+			             {name = "refined-concrete", position = {x = -entrance_width / 2 + i, y = size_y - 3 + a}})
+			table.insert(concrete_positions,
+			             {name = "refined-concrete", position = {x = -entrance_width / 2 + i, y = -size_y + 2 - a}})
 			table.insert(landfill_positions, {name = "landfill", position = {x = -entrance_width / 2 + i, y = size_y - 3 + a}})
 			table.insert(landfill_positions, {name = "landfill", position = {x = -entrance_width / 2 + i, y = -size_y + 2 - a}})
 		end
@@ -86,7 +91,7 @@ local function generate_turtle(moat_width, entrance_width, size_x, size_y)
 
 end
 
-local function generate_infinity_chest(separate_chests, gap, eq)
+local function generate_infinity_chest(separate_chests, operable, gap, eq)
 	local surface = game.surfaces[global.bb_surface_name]
 	local position_0 = {x = 0, y = -42}
 
@@ -94,6 +99,11 @@ local function generate_infinity_chest(separate_chests, gap, eq)
 	for _, object in pairs(objects) do object.destroy() end
 
 	game.print("Special game Infinity chest is being generated!", Color.warning)
+	if operable == "left" then
+		operable = true
+	else
+		operable = false
+	end
 
 	if separate_chests == "left" then
 		local chest = surface.create_entity {
@@ -103,7 +113,7 @@ local function generate_infinity_chest(separate_chests, gap, eq)
 			fast_replace = true
 		}
 		chest.minable = false
-		chest.operable = false
+		chest.operable = operable
 		chest.destructible = false
 		for i, v in ipairs(eq) do
 			chest.set_infinity_container_filter(i, {name = v, index = i, count = game.item_prototypes[v].stack_size})
@@ -120,7 +130,7 @@ local function generate_infinity_chest(separate_chests, gap, eq)
 				fast_replace = true
 			}
 			chest.minable = false
-			chest.operable = false
+			chest.operable = operable
 			chest.destructible = false
 			chest.set_infinity_container_filter(i, {name = v, index = i, count = game.item_prototypes[v].stack_size})
 			chest.clone {position = {position_0.x, -position_0.y}}
@@ -132,13 +142,13 @@ end
 
 local create_special_games_panel = (function(player, frame)
 	frame.clear()
-	frame.add{type = "label", caption = "Configure and apply a simple special game here"}.style.single_line = false
+	frame.add{type = "label", caption = "Configure and apply special games here"}.style.single_line = false
 
 	for k, v in pairs(valid_special_games) do
 		local a = frame.add {type = "frame"}
 		a.style.width = 750
 		local table = a.add {name = k, type = "table", column_count = 3, draw_vertical_lines = true}
-		table.add{type = "label", caption = v.name}.style.width = 100
+		table.add(v.name).style.width = 100
 		local config = table.add {name = k .. "_config", type = "flow", direction = "horizontal"}
 		config.style.width = 500
 		for _, i in ipairs(v.config) do
@@ -157,15 +167,15 @@ local function on_gui_click(event)
 	local config = element.parent.children[2]
 
 	if string.find(element.name, "_apply") then
-		local flow = element.parent.add{type="flow", direction="vertical"}
-		flow.add{type = "button", name = string.gsub(element.name, "_apply", "_confirm"), caption = "Confirm"}
-		flow.add{type = "button", name = "cancel", caption = "Cancel"}
-		element.visible = false	--hides Apply button	
+		local flow = element.parent.add {type = "flow", direction = "vertical"}
+		flow.add {type = "button", name = string.gsub(element.name, "_apply", "_confirm"), caption = "Confirm"}
+		flow.add {type = "button", name = "cancel", caption = "Cancel"}
+		element.visible = false -- hides Apply button	
 		player.print("[SPECIAL GAMES] Are you sure? This change will be reversed only on map restart!", Color.cyan)
 
 	elseif string.find(element.name, "_confirm") then
 		config = element.parent.parent.children[2]
-				
+
 	end
 	-- Insert logic for apply button here
 
@@ -183,27 +193,32 @@ local function on_gui_click(event)
 		local entrance_width = config["entrance_width"].text
 		local size_x = config["size_x"].text
 		local size_y = config["size_y"].text
-	
-		game.forces["spectator"].chart(game.surfaces[global.bb_surface_name], {{-size_x/2-moat_width, -size_y-moat_width}, {size_x/2+moat_width, size_y+moat_width}})
+
+		game.forces["spectator"].chart(game.surfaces[global.bb_surface_name], {
+			{-size_x / 2 - moat_width, -size_y - moat_width}, {size_x / 2 + moat_width, size_y + moat_width}
+		})
 
 	elseif element.name == "infinity_chest_confirm" then
 
 		local separate_chests = config["separate_chests"].switch_state
+		local operable = config["operable"].switch_state
 		local gap = config["gap"].text
 		local eq = {
 			config["eq1"].elem_value, 
 			config["eq2"].elem_value, 
 			config["eq3"].elem_value, 
 			config["eq4"].elem_value,
-			config["eq5"].elem_value
+			config["eq5"].elem_value,
+			config["eq6"].elem_value,
+			config["eq7"].elem_value
 		}
 
-		generate_infinity_chest(separate_chests, gap, eq)
+		generate_infinity_chest(separate_chests, operable, gap, eq)
 
 	end
 	if string.find(element.name, "_confirm") or element.name == "cancel" then
-		element.parent.parent.children[3].visible=true	--shows back Apply button
-		element.parent.destroy()	--removes confirm/Cancel buttons
+		element.parent.parent.children[3].visible = true -- shows back Apply button
+		element.parent.destroy() -- removes confirm/Cancel buttons
 	end
 end
 comfy_panel_tabs['Special games'] = {gui = create_special_games_panel, admin = true}
