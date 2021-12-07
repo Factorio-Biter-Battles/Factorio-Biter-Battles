385a2845b15d651abed57d084cda35e1cfc2f190
patrol <patrol@neutrino.re>
2021-12-07 22:47:13 +0100

    Set path_finder settings
    Suggestion by DrButtons: https://discord.com/channels/823696400797138974/823771633021747210/914712388194959401

--- maps/biter_battles_v2/init.lua
+++ maps/biter_battles_v2/init.lua
@@ -12,6 +12,16 @@ function Public.initial_setup()
 	game.map_settings.pollution.enabled = false
 	game.map_settings.enemy_expansion.enabled = false
 
+	game.map_settings.path_finder.fwd2bwd_ratio = 2
+	game.map_settings.path_finder.goal_pressure_ratio = 3
+	game.map_settings.path_finder.short_cache_size = 30
+	game.map_settings.path_finder.long_cache_size = 50
+	game.map_settings.path_finder.short_cache_min_cacheable_distance = 8
+	game.map_settings.path_finder.long_cache_min_cacheable_distance = 60
+	game.map_settings.path_finder.max_clients_to_accept_any_new_request = 4
+	game.map_settings.path_finder.max_clients_to_accept_short_new_request = 150
+	game.map_settings.path_finder.start_to_goal_cost_multiplier_to_terminate_path_find = 10000
+
 	game.create_force("north")
 	game.create_force("south")
 	game.create_force("north_biters")
