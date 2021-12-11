bb9450cf2f23b93289545b4a53038bc17fcb82e6
MathieuB8 <MathieuB8@users.noreply.github.com>
2021-12-11 09:47:36 +0100

    First draft of changelogs

---
+++ maps/biter_battles_v2/changelogs_tab.lua
@@ -0,0 +1,223 @@
+-- changelogs tab -- 
+local Tabs = require 'comfy_panel.main'
+
+local function add_changelogs(player, element)
+	local changelogs_scrollpanel = element.add { type = "scroll-pane", name = "scroll_pane", direction = "vertical", horizontal_scroll_policy = "never", vertical_scroll_policy = "auto"}
+	changelogs_scrollpanel.style.maximal_height = 530
+	
+	local changelogs_change = {"12/11/2021","Changelogs implementation","Ragnarok77","05/11/2021","implemented various things","Authorr","02/11/2021","added nades","User231","01/11/2021","added bomb","User131"}
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	table.insert(changelogs_change, "xx/yy/2021")
+	table.insert(changelogs_change, "stuffs being implemented")
+	table.insert(changelogs_change, "user143")
+	
+	local t = changelogs_scrollpanel.add { type = "table", name = "changelogs_header_table", column_count = 3 }
+	local column_widths = {tonumber(115), tonumber(435), tonumber(230)}
+	local headers = {
+		[1] = "Date",
+		[2] = "Change",
+		[3] = "Author",
+	}
+	for _, w in ipairs(column_widths) do
+	local label = t.add { type = "label", caption = headers[_] }
+	label.style.minimal_width = w
+	label.style.maximal_width = w
+	label.style.font = "default-bold"
+	label.style.font_color = { r=0.98, g=0.66, b=0.22 }
+	end
+	changelogs_panel_table = changelogs_scrollpanel.add { type = "table", column_count = 3 }
+	if changelogs_change then
+		for i = 1, #changelogs_change, 3 do
+			local label = changelogs_panel_table.add { type = "label", name = "changelogs_date" .. i, caption = changelogs_change[i] }
+			label.style.minimal_width = column_widths[1]
+			label.style.maximal_width = column_widths[1]
+			local label = changelogs_panel_table.add { type = "label", name = "changelogs_change" .. i, caption = changelogs_change[i+1] }
+			label.style.minimal_width = column_widths[2]
+			label.style.maximal_width = column_widths[2]
+			local label = changelogs_panel_table.add { type = "label", name = "changelogs_author" .. i, caption = changelogs_change[i+2] }
+			label.style.minimal_width = column_widths[3]
+			label.style.maximal_width = column_widths[3]
+		end
+	end
+end
+
+function comfy_panel_get_active_frame(player)
+	if not player.gui.left.comfy_panel then return false end
+	if not player.gui.left.comfy_panel.tabbed_pane.selected_tab_index then return player.gui.left.comfy_panel.tabbed_pane.tabs[1].content end
+	return player.gui.left.comfy_panel.tabbed_pane.tabs[player.gui.left.comfy_panel.tabbed_pane.selected_tab_index].content 
+end
+
+local build_config_gui = (function (player, frame)		
+	local frame_changelogs = comfy_panel_get_active_frame(player)
+	if not frame_changelogs then
+		return
+	end
+	frame_changelogs.clear()
+	add_changelogs(player, frame_changelogs)
+end)
+
+comfy_panel_tabs["Changelogs"] = {gui = build_config_gui, admin = false}
