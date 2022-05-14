c951361225c34b2dcaec33684c78a73d5ff07c0b
Ragnarok77 <95619848+Ragnarok77-factorio@users.noreply.github.com>
2022-05-14 22:57:46 +0200

    Changed from character to player for opened (both works though)

--- utils/datastore/jail_data.lua
+++ utils/datastore/jail_data.lua
@@ -378,7 +378,7 @@ local jail = function(player, griefer, msg)
 
     game.players[griefer].clear_console()
     Utils.print_to(griefer, message)
-    game.players[griefer].character.opened = none
+    game.players[griefer].opened = none
     return true
 end
 
