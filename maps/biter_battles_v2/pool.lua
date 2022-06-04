c23bbb4094d8ec1bc6f7e612ff4fc4b1eed5d5b8
gsquaredxc <gsquaredxc@gmail.com>
2022-06-04 14:11:03 -0500

    Rewrite pool based on factorio array implementation

--- maps/biter_battles_v2/pool.lua
+++ maps/biter_battles_v2/pool.lua
@@ -5,11 +5,10 @@ function mod.malloc(size)
 	-- Force allocates an array with hard size limit, then
 	-- returns reference to it. The 'memory' here is just a representation
 	-- layer that emulates cells within RAM.
+	-- Size preferably should be a power of 2 and MUST be less than 1024.
 	local memory = {}
 
-	for i = 1, size, 1 do
-		memory[i] = 0
-	end
+	memory[size] = 0
 
 	return memory
 end
@@ -17,15 +16,14 @@ end
 function mod.enlarge(memory, offset, bytes)
 	-- Resizes memory from offset by bytes. The offset should point at
 	-- last cell of allocated region to prevent overwriting.
-	if memory[offset + 1] ~= nil then
+	-- offset times a power of 2 should equal bytes, including 2^0.
+	if memory[offset + bytes] ~= nil then
 		log("pool::enlarge: writing over allocated region!")
 		local detail_fmt = "pool::enlarge: offset = %d, request = %d"
 		log(string.format(detail_fmt, offset, bytes))
 	end
 
-	for i = offset + 1, offset + bytes, 1 do
-		memory[i] = 0
-	end
+	memory[offset + bytes] = 0
 
 	return memory
 end
