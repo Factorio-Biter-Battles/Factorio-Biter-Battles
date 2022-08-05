6971819f3854c9311a2b40ff8ceb2873658a8858
developer <88335092+developer-8@users.noreply.github.com>
2022-08-05 14:59:55 +0300

    fix ghost rotation by spectator (#297)
    Co-authored-by: developer-8 <>

--- maps/biter_battles_v2/init.lua
+++ maps/biter_battles_v2/init.lua
@@ -99,7 +99,6 @@ function Public.initial_setup()
 		defines.input_action.open_kills_gui,
 		defines.input_action.quick_bar_set_selected_page,
 		defines.input_action.quick_bar_set_slot,
-		defines.input_action.rotate_entity,
 		defines.input_action.set_filter,
 		defines.input_action.set_player_color,
 		defines.input_action.start_walking,
