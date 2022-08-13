276982da4a84eba588bc0bd062da8cdf773f2246
Ragnarok77 <95619848+Ragnarok77-factorio@users.noreply.github.com>
2022-08-13 16:15:14 +0200

    Removed ,

--- maps/biter_battles_v2/init.lua
+++ maps/biter_battles_v2/init.lua
@@ -209,9 +209,9 @@ function Public.tables()
 	global.active_special_games = {}
 	global.special_games_variables = {}
 	global.player_data_afk = {}
-	global.max_group_size_initial = 300,							--Maximum unit group size for all biters at start, just used as a reference, doesnt change initial group size.
-	global.max_group_size_north = 300,							--Maximum unit group size for north biters.
-	global.max_group_size_south = 300,							--Maximum unit group size for south biters.
+	global.max_group_size_initial = 300							--Maximum unit group size for all biters at start, just used as a reference, doesnt change initial group size.
+	global.max_group_size_north = 300							--Maximum unit group size for north biters.
+	global.max_group_size_south = 300							--Maximum unit group size for south biters.
 	global.biter_spawn_unseen = {
 		["north"] = {
 			["medium-spitter"] = true, ["medium-biter"] = true, ["big-spitter"] = true, ["big-biter"] = true, ["behemoth-spitter"] = true, ["behemoth-biter"] = true
