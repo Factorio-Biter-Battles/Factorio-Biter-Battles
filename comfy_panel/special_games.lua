945613ac18301f047b5fa5b4eaa18309b757970b
developer <88335092+developer-8@users.noreply.github.com>
2022-11-20 20:29:01 +0200

    Update comfy_panel/special_games.lua
    Co-authored-by: TheBigZet <92057893+TheBigZet@users.noreply.github.com>

--- comfy_panel/special_games.lua
+++ comfy_panel/special_games.lua
@@ -278,13 +278,14 @@ local function generate_limited_lives(lives_limit)
 	end
 
 	global.active_special_games["limited_lives"] = true
-	global.special_games_variables["limited_lives"] = {}
-	global.special_games_variables["limited_lives"]["lives_limit"] = lives_limit
-	global.special_games_variables["limited_lives"]["player_lives"] = {}
-	global.special_games_variables["limited_lives"]["has_life"] = function (player_name)
-		local player_lives = global.special_games_variables["limited_lives"]["player_lives"][player_name]
-		return player_lives == nil or player_lives > 0
-	end
+	global.special_games_variables["limited_lives"] = {
+		lives_limit = lives_limit,
+		player_lives = {},
+		has_life = function (player_name)
+			local player_lives = global.special_games_variables["limited_lives"]["player_lives"][player_name]
+			return player_lives == nil or player_lives > 0
+		end
+	}
 	local special_game_description = table.concat({"Each player has only", lives_limit , ((lives_limit == 1) and "life" or "lives"), "until the end of the game."}, " ")
 	global.special_games_variables["limited_lives"]["text_id"] = rendering.draw_text{
 		text = special_game_description,
