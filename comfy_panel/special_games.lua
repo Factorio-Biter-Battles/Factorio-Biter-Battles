496223f6fea259e725d84367727369323f47d025
PlayerNoon <120744927+PlayerNoon@users.noreply.github.com>
2023-01-23 11:35:50 +0100

    change names to images

--- comfy_panel/special_games.lua
+++ comfy_panel/special_games.lua
@@ -97,13 +97,20 @@ local valid_special_games = {
 		disable_sciences = {
 		name = {type = "label", caption = "Disable sciences", tooltip = "disable sciences that players wont be able to send."},
 		config = {
-			[1] = {name = "red", type = "checkbox", caption = "red", state = false},
-			[2] = {name = "green", type = "checkbox", caption = "green", state = false},
-			[3] = {name = "gray", type = "checkbox", caption = "gray", state = false},
-			[4] = {name = "blue", type = "checkbox", caption = "blue", state = false},
-			[5] = {name = "purple", type = "checkbox", caption = "purple", state = false},
-			[6] = {name = "yellow", type = "checkbox", caption = "yellow", state = false},
-			[7] = {name = "white", type = "checkbox", caption = "white", state = false}
+			[1] = {name = "red", type = "checkbox", state = false},
+			[2] = {name = "1", type = "sprite", sprite = "item/automation-science-pack"},
+			[3] = {name = "green", type = "checkbox", state = false},
+			[4] = {name = "2", type = "sprite", sprite = "item/logistic-science-pack"},
+			[5] = {name = "gray", type = "checkbox", state = false},
+			[6] = {name = "3", type = "sprite", sprite = "item/military-science-pack"},
+			[7] = {name = "blue", type = "checkbox", state = false},
+			[8] = {name = "4", type = "sprite", sprite = "item/chemical-science-pack"},
+			[9] = {name = "purple", type = "checkbox",  state = false},
+			[10] = {name = "5", type = "sprite", sprite = "item/production-science-pack"},
+			[11] = {name = "yellow", type = "checkbox", state = false},
+			[12] = {name = "6", type = "sprite", sprite = "item/utility-science-pack"},
+			[13] = {name = "white", type = "checkbox", state = false},
+			[14] = {name = "7", type = "sprite", sprite = "item/space-science-pack"}
 		},
 		button = {name = "disable_sciences_apply", type = "button", caption = "Apply"}
 	}
