c73622b8cb4647ee5b01afe8f8be3c613bc15064
Elliott Hughes <elliott.huge@gmail.com>
2023-03-05 12:33:48 +0000

    #348 Tweak cleared area, remove duped loop

--- maps/biter_battles_v2/terrain.lua
+++ maps/biter_battles_v2/terrain.lua
@@ -586,13 +586,7 @@ function Public.generate_silo(surface)
 		create_mirrored_tile_chain(surface, {name = "stone-path", position = silo.position}, 32, 10)
 	end
 	
-	for _, entity in pairs(surface.find_entities({{pos.x - 4, pos.y - 4}, {pos.x + 4, pos.y + 4}})) do
-		if entity.type == "simple-entity" or entity.type == "tree" or entity.type == "resource" then
-			entity.destroy()
-		end
-	end
-	
-	for _, entity in pairs(surface.find_entities({{pos.x - 4, pos.y - 6}, {pos.x + 4, pos.y + 6}})) do
+	for _, entity in pairs(surface.find_entities({{pos.x - 4, pos.y - 5}, {pos.x + 5, pos.y + 6}})) do
 		if entity.type == "simple-entity" or entity.type == "tree" or entity.type == "resource" then
 			entity.destroy()
 		end
