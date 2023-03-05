4727bf92fe22829c6813e53a29a16faf5ba20431
Elliott Hughes <elliott.huge@gmail.com>
2023-03-05 12:18:45 +0000

    #348 Clear a larger area around Silo for Turrets
    Resolves issue #348

--- maps/biter_battles_v2/terrain.lua
+++ maps/biter_battles_v2/terrain.lua
@@ -586,15 +586,14 @@ function Public.generate_silo(surface)
 		create_mirrored_tile_chain(surface, {name = "stone-path", position = silo.position}, 32, 10)
 	end
 	
-	local p = silo.position
-	for _, entity in pairs(surface.find_entities({{p.x - 4, p.y - 4}, {p.x + 4, p.y + 4}})) do
+	for _, entity in pairs(surface.find_entities({{pos.x - 6, pos.y - 6}, {pos.x + 6, pos.y + 6}})) do
 		if entity.type == "simple-entity" or entity.type == "tree" or entity.type == "resource" then
 			entity.destroy()
 		end
 	end
-	local turret1 = surface.create_entity({name = "gun-turret", position = {x=pos.x, y=pos.y-5}, force = "north"})
+	local turret1 = surface.create_entity({name = "gun-turret", position = {x=pos.x, y=pos.y - 5}, force = "north"})
 	turret1.insert({name = "firearm-magazine", count = 10})
-	local turret2 = surface.create_entity({name = "gun-turret", position = {x=pos.x+2, y=pos.y-5}, force = "north"})
+	local turret2 = surface.create_entity({name = "gun-turret", position = {x=pos.x + 2, y=pos.y - 5}, force = "north"})
 	turret2.insert({name = "firearm-magazine", count = 10})
 end
 --[[
