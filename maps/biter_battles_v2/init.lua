8dc6a202f1b896a97a2fed967017bac0229792a1
TheBigZet <92057893+TheBigZet@users.noreply.github.com>
2023-07-16 16:06:39 +0200

    Fix merge

--- maps/biter_battles_v2/init.lua
+++ maps/biter_battles_v2/init.lua
@@ -215,8 +215,6 @@ function Public.tables()
 	global.unit_spawners.south_biters = {}
 	global.ai_strikes = {}
 	global.ai_targets = {}
-	global.active_special_games = {}
-	global.special_games_variables = {}
 
 	global.player_data_afk = {}
 	global.max_group_size_initial = 300							--Maximum unit group size for all biters at start, just used as a reference, doesnt change initial group size.
