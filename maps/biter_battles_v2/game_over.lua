ba67448f933f6fe44267a6c0d53b9485d96d6d08
Ragnarok77 <95619848+Ragnarok77-factorio@users.noreply.github.com>
2023-10-14 21:51:04 +0200

    bugfix : missing adding difficulty in log result file of match

--- maps/biter_battles_v2/game_over.lua
+++ maps/biter_battles_v2/game_over.lua
@@ -499,6 +499,7 @@ function Public.silo_death(event)
 			log_to_db('[Gamelength]'..game.ticks_played..'\n',true)
 			log_to_db('[StartTick]'..global.special_games_variables["captain_mode"]["stats"]["tickGameStarting"]..'\n',true)
 			log_to_db('[WinnerTeam]'..global.bb_game_won_by_team..'\n',true)
+			log_to_db('[ExtraInfo]'..global.special_games_variables["captain_mode"]["stats"]["extrainfo"]..'\n',true)
 			for _, player in pairs(game.players) do
 				if player.connected and (player.force.name == "north" or player.force.name == "south") then
 					Special_games.captain_log_end_time_player(player)
