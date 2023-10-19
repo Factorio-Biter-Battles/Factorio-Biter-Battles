6b1c2f1635eb580a4c9b8c36462e810e92d81ab9
Ragnarok77 <95619848+Ragnarok77-factorio@users.noreply.github.com>
2023-10-19 18:26:36 +0200

    Update changelog_tab.lua

--- maps/biter_battles_v2/changelog_tab.lua
+++ maps/biter_battles_v2/changelog_tab.lua
@@ -7,6 +7,9 @@ local function add_changelog(player, element)
 	
 	local changelog_change = {}
         table.insert(changelog_change,"2023-10-19")
+        table.insert(changelog_change,"perf : Increase refresh threat min delay and optim on code")
+        table.insert(changelog_change,"XVhc6A")
+        table.insert(changelog_change,"2023-10-19")
         table.insert(changelog_change,"perf : biter terrain tiles optim")
         table.insert(changelog_change,"cogito123")
         table.insert(changelog_change,"2023-10-19")
