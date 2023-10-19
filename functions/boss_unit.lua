6e7be2534a274a4029d6cce07452fa4c25d5da1b
Ragnarok77 <95619848+Ragnarok77-factorio@users.noreply.github.com>
2023-10-19 22:22:03 +0200

    Merge PR400 : Ignore damage events for non-biters and increase refresh delay for healthbar of bosses

--- functions/boss_unit.lua
+++ functions/boss_unit.lua
@@ -30,8 +30,6 @@ end
 
 local function on_entity_damaged(event)
 	local entity = event.entity
-	if not entity.valid then return end
-	if entity.type ~= "unit" then return end
 	local boss = global.boss_units[entity.unit_number]
 	if not boss then return end
 	entity.health = entity.health + event.final_damage_amount
@@ -40,7 +38,7 @@ local function on_entity_damaged(event)
 		global.boss_units[entity.unit_number] = nil
 		entity.die()
 	else
-		if boss.last_update + 10 < game.tick then
+		if boss.last_update + 30 < game.tick then
 			set_healthbar(global.boss_units[entity.unit_number])
 			boss.last_update = game.tick
 		end
@@ -54,4 +52,9 @@ end
 local event = require 'utils.event'
 event.on_init(on_init)
 event.add(defines.events.on_entity_damaged, on_entity_damaged)
+event.add_event_filter(defines.events.on_entity_damaged, {
+	filter = "type",
+	type = "unit",
+})
+
 return Public
