7ff127664773b99ed77be531c89c90e36789ae3a
Ragnarok77 <95619848+Ragnarok77-factorio@users.noreply.github.com>
2023-10-27 10:17:40 +0200

    PR402 bugfix ignore

--- maps/biter_battles_v2/main.lua
+++ maps/biter_battles_v2/main.lua
@@ -63,6 +63,39 @@ local function on_console_chat(event)
 	Functions.share_chat(event)
 end
 
+local function on_console_command(event)
+	local cmd = event.command
+    if not event.player_index then return end
+    local player = game.players[event.player_index]
+    local param = event.parameters
+    if cmd == "ignore" then 
+		if param and not string.match(param, "[ '\";`]") and not param:match("^%s*$") then
+			if not global.ignore_lists[player.name] then
+				global.ignore_lists[player.name] = {}
+			end
+			if not global.ignore_lists[player.name][param] then
+				global.ignore_lists[player.name][param] = true
+				player.print("You have ignored " .. param, {r = 0, g = 1, b = 1})
+			else
+				player.print("You are already ignoring " .. param, {r = 0, g = 1, b = 1})
+			end
+		else
+			player.print("Invalid input. Make sure the name contains no spaces, quotes, semicolons, backticks, or any spaces.", {r = 1, g = 0, b = 0})
+		end
+    elseif cmd == "unignore" then 
+		if param and not string.match(param, "[ '\";`]") and not param:match("^%s*$") and global.ignore_lists[player.name] then
+			if global.ignore_lists[player.name][param] then
+                global.ignore_lists[player.name][param] = nil
+                player.print("You have unignored " .. param, {r = 0, g = 1, b = 1})
+            else
+                player.print("You are not currently ignoring " .. param, {r = 0, g = 1, b = 1})
+            end
+		else
+			player.print("Invalid input. Make sure the name contains no spaces, quotes, semicolons, backticks, or any spaces.", {r = 1, g = 0, b = 0})
+		end
+	end
+end
+
 local function on_built_entity(event)
 	Functions.no_landfill_by_untrusted_user(event)
 	Functions.no_turret_creep(event)
@@ -393,6 +426,7 @@ Event.add(defines.events.on_entity_cloned, on_entity_cloned)
 Event.add(defines.events.on_built_entity, on_built_entity)
 Event.add(defines.events.on_chunk_generated, on_chunk_generated)
 Event.add(defines.events.on_console_chat, on_console_chat)
+Event.add(defines.events.on_console_command, on_console_command)
 Event.add(defines.events.on_entity_died, on_entity_died)
 Event.add(defines.events.on_ai_command_completed, on_ai_command_completed)
 Event.add(defines.events.on_gui_click, on_gui_click)
