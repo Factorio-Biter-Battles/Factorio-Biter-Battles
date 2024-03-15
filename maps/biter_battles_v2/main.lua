Factorio-Biter-Battles/Factorio-Biter-Battles

commit e8fd443543cf722375bfff98a7ace8f042e98ee6

functions.lua: make this file more "pure"
Specifically, with these changes (which are functionally a no-op), it
is now possible to load functions.lua outside of factorio (i.e. there
will be no access to gloabls/script/_STAGE/etc as part of loading the
file). This is being done to make it possible to write unit tests of
the functionality in functions.lua that run from the lua commandline
utiility.

This also marks a few more functions as local.

maps/biter_battles_v2/main.lua
@@ -62,7 +62,44 @@ local function on_research_finished(event)
 end
 
 local function on_console_chat(event)
-	Functions.share_chat(event)
+	--Share chat with spectator force
+	if not event.message or not event.player_index then return end
+	local player = game.players[event.player_index]
+	local player_name = player.name
+	local player_force_name = player.force.name
+	local tag = player.tag
+	if not tag then tag = "" end
+	local color = player.chat_color
+
+	local muted = Muted.is_muted(player_name)
+	local mute_tag = ""
+	if muted then
+		mute_tag = "[muted] "
+	end
+
+	local msg = player_name .. tag .. " (" .. player_force_name .. "): ".. event.message
+	if not muted and (player_force_name == "north" or player_force_name == "south") then
+		Public.print_message_to_players(game.forces.spectator.players,player_name,msg,color)
+	end
+
+	if global.tournament_mode and not player.admin then return end
+
+	--Skip messages that would spoil coordinates from spectators and don't send gps coord to discord
+	local a, b = string_find(event.message, "gps=", 1, false)
+	if a then return end
+
+	local discord_msg = ""
+	if muted then
+		discord_msg = mute_tag
+		Muted.print_muted_message(player)
+	end
+	if not muted and player_force_name == "spectator" then
+		Public.print_message_to_players(game.forces.north.players,player_name,msg,nil)
+		Public.print_message_to_players(game.forces.south.players,player_name,msg,nil)
+	end
+
+	discord_msg = discord_msg .. player_name .. " (" .. player_force_name .. "): ".. event.message
+	Server.to_discord_player_chat(discord_msg)
 end
 
 local function on_console_command(event)
@@ -101,7 +138,7 @@ local function on_console_command(event)
 end
 
 local function on_built_entity(event)
-	Functions.no_landfill_by_untrusted_user(event)
+	Functions.no_landfill_by_untrusted_user(event, Session.get_trusted_table())
 	Functions.no_turret_creep(event)
 	Terrain.deny_enemy_side_ghosts(event)
 	AiTargets.start_tracking(event.created_entity)

maps/biter_battles_v2/functions.lua
@@ -1,7 +1,4 @@
-local Server = require 'utils.server'
-local Muted = require 'utils.muted'
 local Tables = require "maps.biter_battles_v2.tables"
-local Session = require 'utils.datastore.session_data'
 local bb_config = require "maps.biter_battles_v2.config"
 local simplex_noise = require 'utils.simplex_noise'.d2
 local string_sub = string.sub
@@ -12,6 +9,28 @@ local math_min = math.min
 local math_floor = math.floor
 local string_find = string.find
 local gui_style = require 'utils.utils'.gui_style
+
+local function get_ammo_modifier(ammo_category)
+	local result = 0
+	if Tables.base_ammo_modifiers[ammo_category] then
+        result = Tables.base_ammo_modifiers[ammo_category]
+	end
+    return result
+end
+local function get_turret_attack_modifier(turret_category)
+	local result = 0
+	if Tables.base_turret_attack_modifiers[turret_category] then
+        result = Tables.base_turret_attack_modifiers[turret_category]
+	end
+    return result
+end
+local function get_upgrade_modifier(ammo_category)
+    result = 0
+    if Tables.upgrade_modifiers[ammo_category] then
+        result = Tables.upgrade_modifiers[ammo_category]
+    end
+    return result
+end
 
 -- Only add upgrade research balancing logic in this section
 -- All values should be in tables.lua
@@ -306,12 +325,11 @@ function Public.no_turret_creep(event)
 	entity.destroy()
 end
 
-function Public.no_landfill_by_untrusted_user(event)
+function Public.no_landfill_by_untrusted_user(event, trusted_table)
 	local entity = event.created_entity
 	if not entity.valid or not event.player_index or entity.name ~= "tile-ghost" or entity.ghost_name ~= "landfill" then return end
 	local player = game.players[event.player_index]
-	local trusted = Session.get_trusted_table()
-	if not trusted[player.name] then
+	if not trusted_table[player.name] then
 		player.print('You have not grown accustomed to this technology yet.', {r = 0.22, g = 0.99, b = 0.99})
 		entity.destroy()
 		return
@@ -353,47 +371,6 @@ function Public.print_message_to_players(forcePlayerList, playerNameSendingMessa
 			end
 		end
 	end
-end
-
---Share chat with spectator force
-function Public.share_chat(event)
-	if not event.message or not event.player_index then return end
-	local player = game.players[event.player_index]
-	local player_name = player.name
-	local player_force_name = player.force.name
-	local tag = player.tag
-	if not tag then tag = "" end
-	local color = player.chat_color
-	
-	local muted = Muted.is_muted(player_name)
-	local mute_tag = ""
-	if muted then 
-		mute_tag = "[muted] "
-	end
-
-	local msg = player_name .. tag .. " (" .. player_force_name .. "): ".. event.message
-	if not muted and (player_force_name == "north" or player_force_name == "south") then
-		Public.print_message_to_players(game.forces.spectator.players,player_name,msg,color)
-	end
-
-	if global.tournament_mode and not player.admin then return end
-
-	--Skip messages that would spoil coordinates from spectators and don't send gps coord to discord
-	local a, b = string_find(event.message, "gps=", 1, false)
-	if a then return end
-
-	local discord_msg = ""
-	if muted then 
-		discord_msg = mute_tag
-		Muted.print_muted_message(player)
-	end 
-	if not muted and player_force_name == "spectator" then
-		Public.print_message_to_players(game.forces.north.players,player_name,msg,nil)
-		Public.print_message_to_players(game.forces.south.players,player_name,msg,nil)
-	end
-	
-	discord_msg = discord_msg .. player_name .. " (" .. player_force_name .. "): ".. event.message
-	Server.to_discord_player_chat(discord_msg)
 end
 
 function Public.spy_fish(player, event)
@@ -475,29 +452,6 @@ function Public.map_intro_click(player, element)
 			return true
 		end
 	end	
-end
-
-function get_ammo_modifier(ammo_category)
-	local result = 0
-	if Tables.base_ammo_modifiers[ammo_category] then
-        result = Tables.base_ammo_modifiers[ammo_category]
-	end
-    return result
-end
-function get_turret_attack_modifier(turret_category)
-	local result = 0
-	if Tables.base_turret_attack_modifiers[turret_category] then
-        result = Tables.base_turret_attack_modifiers[turret_category]
-	end
-    return result
-end
-
-function get_upgrade_modifier(ammo_category)
-    result = 0
-    if Tables.upgrade_modifiers[ammo_category] then
-        result = Tables.upgrade_modifiers[ammo_category]
-    end
-    return result
 end
 
 return Public

commit b03f2412c08f27eb0c03ab841b653df677bef396

Fix chat messages not showing up in Discord
This fixes issues introduced in e8fd443543cf722375bfff98a7ace8f042e98ee6.

Thank you to TSP for the clear bug report!

maps/biter_battles_v2/main.lua
@@ -9,10 +9,12 @@ local Game_over = require "maps.biter_battles_v2.game_over"
 local Gui = require "maps.biter_battles_v2.gui"
 local Init = require "maps.biter_battles_v2.init"
 local Mirror_terrain = require "maps.biter_battles_v2.mirror_terrain"
+local Muted = require "utils.muted"
 require 'modules.simple_tags'
 local Team_manager = require "maps.biter_battles_v2.team_manager"
 local Terrain = require "maps.biter_battles_v2.terrain"
 local Session = require 'utils.datastore.session_data'
+local Server = require 'utils.server'
 local Color = require 'utils.color_presets'
 local autoTagWestOutpost = "[West]"
 local autoTagEastOutpost = "[East]"
@@ -87,13 +89,13 @@ local function on_console_chat(event)
 
 	local msg = player_name .. tag .. " (" .. player_force_name .. "): ".. event.message
 	if not muted and (player_force_name == "north" or player_force_name == "south") then
-		Public.print_message_to_players(game.forces.spectator.players,player_name,msg,color)
+		Functions.print_message_to_players(game.forces.spectator.players,player_name,msg,color)
 	end
 
 	if global.tournament_mode and not player.admin then return end
 
 	--Skip messages that would spoil coordinates from spectators and don't send gps coord to discord
-	local a, b = string_find(event.message, "gps=", 1, false)
+	local a, b = string.find(event.message, "gps=", 1, false)
 	if a then return end
 
 	local discord_msg = ""
@@ -102,8 +104,8 @@ local function on_console_chat(event)
 		Muted.print_muted_message(player)
 	end
 	if not muted and player_force_name == "spectator" then
-		Public.print_message_to_players(game.forces.north.players,player_name,msg,nil)
-		Public.print_message_to_players(game.forces.south.players,player_name,msg,nil)
+		Functions.print_message_to_players(game.forces.north.players,player_name,msg,nil)
+		Functions.print_message_to_players(game.forces.south.players,player_name,msg,nil)
 	end
 
 	discord_msg = discord_msg .. player_name .. " (" .. player_force_name .. "): ".. event.message