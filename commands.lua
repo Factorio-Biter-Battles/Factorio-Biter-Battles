2383684799569aee2954bcde71480b5d85e6731f
DrButtons <DrButtons@DrButtons.com>
2024-03-24 23:39:18 -0400

    Replace force-map-reset w/ instant-map-reset #364
    The force-map-reset command randomly picks a silo and kills it in order
    to trigger the map-reset timer.  This then requires you to wait the 120
    seconds for the map reset, or - more often than not - set the gamespeed
    to 5 and wait for the next map.
    
    The instant-map-reset command essentially serves as a replacement to
    that command, and has the benefits of:
    - Not ending the game and sending mmr stats to the scores server.
    - Is instant (resets the map immediately)
    - Is reproducible (allows you to specify a seed)
    
    Therefore, in the interest of de-bloating the codebase, it makes more
    sense to keep just the instant-map-reset command, and remove the
    force-map-reset command.

--- commands.lua
+++ commands.lua
@@ -1,6 +1,5 @@
-require 'commands.misc'
 require 'commands.where'
 require 'commands.chat_commands'
 require 'commands.difficulty_voting_commands'
-require 'commands.force_map_reset'
+require 'commands.instant_map_reset'
 require 'commands.suspend'
