8a1654061e4670ef2dbf291eabcb955aae7a6174
Moller21 <152439681+Moller21@users.noreply.github.com>
2024-04-04 13:29:11 +0200

    Update boss_unit.lua

--- functions/boss_unit.lua
+++ functions/boss_unit.lua
@@ -1,20 +1,30 @@
 --adds a health bar and health increase to a unit
 local Public = {}
+local healthbartext_playerlist
 
 local function create_healthbar(entity, size)
-	return rendering.draw_sprite({
+	global.next_healthbar_update = entity.unit_number
+
+	local healthbar_id = rendering.draw_sprite({
 		sprite="virtual-signal/signal-white",
 		tint={0, 200, 0},
 		x_scale=size * 15, y_scale=size, render_layer="light-effect",
 		target=entity, target_offset={0, -2.5}, surface=entity.surface,
 	})
-end
 
-local function set_healthbar(boss_unit)
-	local m = boss_unit.health / boss_unit.max_health
-	local x_scale = rendering.get_y_scale(boss_unit.healthbar_id) * 15
-	rendering.set_x_scale(boss_unit.healthbar_id, x_scale * m)
-	rendering.set_color(boss_unit.healthbar_id, {math.floor(255 - 255 * m), math.floor(200 * m), 0})
+	local healthtext_id = rendering.draw_text({
+		text = 0,
+		surface=entity.surface,
+		target=entity,
+		target_offset={0, -3},
+		scale = 1.5,
+		alignment='center',
+		color={1, 1, 1, 1},
+		players=healthbartext_playerlist, --need to do this because empty playerlist means no filter and cannot give it fake names
+		visible=#healthbartext_playerlist > 0,
+	})
+
+	return healthbar_id, healthtext_id
 end
 
 function Public.add_boss_unit(entity, health_factor, size)
@@ -25,10 +35,47 @@ function Public.add_boss_unit(entity, health_factor, size)
 	if health == 0 then return end
 	local s = 0.5
 	if size then s = size end
-	global.boss_units[entity.unit_number] = {entity = entity, max_health = health, health = health, healthbar_id = create_healthbar(entity, s), last_update = game.tick}
+	local healthbar_id, healthtext_id = create_healthbar(entity, s)
+	global.boss_units[entity.unit_number] = {entity = entity, max_health = health, health = health, healthbar_id = healthbar_id, healthtext_id = healthtext_id}
 end
 
----@param event EventData.on_entity_damaged
+function Public.healthbarnumbers_enable_for_player(playerName, enable)
+	local foundIndex, didChanges
+	for i = 1, #healthbartext_playerlist do
+		if healthbartext_playerlist[i] == playerName then
+			foundIndex = i
+			break
+		end
+	end
+
+	if (enable) then
+		if not foundIndex then
+			didChanges = true
+			table.insert(healthbartext_playerlist, playerName)
+		end
+	else
+		if (foundIndex) then
+			didChanges = true
+			table.remove(healthbartext_playerlist, foundIndex)
+		end
+	end
+
+	if didChanges then
+		for _,boss in pairs(global.boss_units) do
+			rendering.set_visible(boss.healthtext_id ,#healthbartext_playerlist>0)
+			rendering.set_players(boss.healthtext_id ,healthbartext_playerlist)
+		end
+	end
+end
+
+function Public.healthbarnumbers_enabled_for_player(playerName)
+	for i = 1, #healthbartext_playerlist do
+		if healthbartext_playerlist[i] == playerName then
+			return true
+		end
+	end
+end
+
 local function on_entity_damaged(event)
 	local entity = event.entity
 	local boss = global.boss_units[entity.unit_number]
@@ -37,26 +84,52 @@ local function on_entity_damaged(event)
 	boss.health = boss.health - event.final_damage_amount
 	if boss.health <= 0 then
 		global.boss_units[entity.unit_number] = nil
-		if event.cause then
-			entity.die(event.cause.force, event.cause)
-		else
-			entity.die()
+		entity.die()
+		if not next(global.boss_units) then
+			global.next_healthbar_update = nil
 		end
-	else
-		if boss.last_update + 30 < game.tick then
-			set_healthbar(global.boss_units[entity.unit_number])
-			boss.last_update = game.tick
+	end
+end
+
+local function on_tick()
+	if global.next_healthbar_update then
+		local current_boss = global.boss_units[global.next_healthbar_update]
+		if current_boss then
+			-- update healthbar
+			local m = current_boss.health / current_boss.max_health
+			local x_scale = rendering.get_y_scale(current_boss.healthbar_id) * 15
+			rendering.set_x_scale(current_boss.healthbar_id, x_scale * m)
+			rendering.set_color(current_boss.healthbar_id, {math.floor(255 - 255 * m), math.floor(200 * m), 0})
+
+			local text = 0
+			if current_boss.health >= 10^6 then
+		        text = string.format("%.2fM", current_boss.health / 10^6)
+		    elseif current_boss.health >= 10^3 then
+		        text = string.format("%.2fK", current_boss.health / 10^3)
+		    else
+		        text = tostring(current_boss.health)
+		    end
+			rendering.set_text(current_boss.healthtext_id, text)
+
+			global.next_healthbar_update = next(global.boss_units, global.next_healthbar_update)
+			if not global.next_healthbar_update then
+				global.next_healthbar_update = next(global.boss_units)
+			end
+		else
+			global.next_healthbar_update = next(global.boss_units)
 		end
 	end
 end
 
 local function on_init()
 	global.boss_units = {}
+	healthbartext_playerlist = {}
 end
 
 local event = require 'utils.event'
 event.on_init(on_init)
 event.add(defines.events.on_entity_damaged, on_entity_damaged)
+event.add(defines.events.on_tick, on_tick)
 event.add_event_filter(defines.events.on_entity_damaged, {
 	filter = "type",
 	type = "unit",
