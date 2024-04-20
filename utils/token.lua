e44a50c3dfcf1ac1f1a276c8800b1b28ab128386
Vadim <129969897+vadcx@users.noreply.github.com>
2024-04-20 21:37:44 +0200

    Comment hardcoded values for _LIFECYCLE checks (#486)
    These come from utils/data_stages.lua but I am not sure how often these
    paths are hit during play, so I decided to leave it at comments for now.
    Just to help understand code better later on.

--- utils/token.lua
+++ utils/token.lua
@@ -11,7 +11,7 @@ local counter = 0
 -- @param  var<any>
 -- @return number the unique token for the variable.
 function Token.register(var)
-    if _LIFECYCLE == 8 then
+    if _LIFECYCLE == 8 then -- Runtime
         error('Calling Token.register after on_init() or on_load() has run is a desync risk.', 2)
     end
 
