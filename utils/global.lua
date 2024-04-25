22b69f42b72d9051280854583046633bba440069
vadcx <vad.git@vad.cx>
2024-04-25 23:22:23 +0200

    utils/global.lua: add doc

--- utils/global.lua
+++ utils/global.lua
@@ -7,6 +7,10 @@ local concat = table.concat
 local names = {}
 Global.names = names
 
+---Registers a function persistently for the `on_load` event (loading from a saved game).
+---@see EventData.on_load
+---@param tbl table Data-only table that will be persistently stored in `global` using given token.
+---@param callback fun(registered_as_token: number)
 function Global.register(tbl, callback)
     if _LIFECYCLE ~= _STAGE.control then
         error('can only be called during the control stage', 2)
@@ -26,6 +30,13 @@ function Global.register(tbl, callback)
     return token
 end
 
+---Registers a function persistently for the `on_load` event (loading from a saved game) and `on_init` (when started for the first time).
+---Otherwise identical to Global.register
+---@see EventData.on_load
+---@see EventData.on_init
+---@param tbl table Data-only table that will be persistently stored in `global` using given token.
+---@param init_handler fun(tbl: table)
+---@param callback fun(registered_as_token: number) | fun(tbl: table) # in on_init, both functions will receive the passed tbl table
 function Global.register_init(tbl, init_handler, callback)
     if _LIFECYCLE ~= _STAGE.control then
         error('can only be called during the control stage', 2)
