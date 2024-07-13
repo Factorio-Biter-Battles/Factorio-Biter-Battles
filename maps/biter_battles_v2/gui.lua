697da698c1d4744cf71143709127cbd20a374694
PlayerNoon <120744927+PlayerNoon@users.noreply.github.com>
2024-07-13 10:00:17 +0200

    fix
    add space after,
    rename inventory to burners_to_drop
    use ~=

--- maps/biter_battles_v2/gui.lua
+++ maps/biter_battles_v2/gui.lua
@@ -374,7 +374,7 @@ local get_player_data = function (player, remove)
 	return global.player_data_afk[player.name]
 end
 
-local function drop_burners(player,forced_join)
+local function drop_burners(player, forced_join)
 	if forced_join then 
 		global.got_burners[player.name] = nil
 		return
@@ -382,10 +382,10 @@ local function drop_burners(player,forced_join)
 	if global.training_mode or not (global.bb_settings.burners_balance) then 		
 		return
 	end			
-	local inventory = player.get_item_count("burner-mining-drill")	
-	if not (inventory == 0) then
-    	local items = player.surface.spill_item_stack(player.position,{name="burner-mining-drill", count = inventory}, false, nil, false )
-		player.remove_item({name="burner-mining-drill", count = inventory})
+	local burners_to_drop = player.get_item_count("burner-mining-drill")	
+	if burners_to_drop ~= 0 then
+    	local items = player.surface.spill_item_stack(player.position,{name="burner-mining-drill", count = burners_to_drop}, false, nil, false )
+		player.remove_item({name="burner-mining-drill", count = burners_to_drop})
 	end
 end
 
@@ -575,7 +575,7 @@ function spectate(player, forced_join, stored_position)
 
 	player.driving = false
 	player.clear_cursor()
-	drop_burners(player,forced_join)
+	drop_burners(player, forced_join)
 
 	if stored_position then
 		local p_data = get_player_data(player)
