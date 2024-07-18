29ac2c910ff5d11c920931f7f8862c1a133528d2
RedRafe <gabri97.gt+redrafe@gmail.com>
2024-07-18 12:21:26 +0200

    Remove duplicate from merge

--- maps/biter_battles_v2/init.lua
+++ maps/biter_battles_v2/init.lua
@@ -219,7 +219,6 @@ function Public.queue_reveal_map()
 		-- spectator island (guarantees sounds to be played during map reveal)
 		q_push(chart_queue, {{-16, -16}, {16, 16}})
 	end
-
 	-- request whole starting area at the end again to clear any charting hiccup
 	q_push(chart_queue, {{-height, -height}, {height, height}})
 end
@@ -379,19 +378,6 @@ function Public.load_spawn()
 		surface.request_to_generate_chunks({x = -16, y = y * -1 - 16}, 0)
 		surface.request_to_generate_chunks({x = -48, y = y * -1 - 16}, 0)
 		surface.request_to_generate_chunks({x = -80, y = y * -1 - 16}, 0)
-	end
-end
-
----@param max_requests number
-function Public.pop_chunk_request(max_requests)
-	max_requests = max_requests or 1
-	local chart_queue = global.chart_queue
-	local surface = game.surfaces[global.bb_surface_name]
-	local spectator = game.forces.spectator
-
-	while max_requests > 0 and q_size(chart_queue) > 0 do
-		spectator.chart(surface, q_pop(chart_queue))
-		max_requests = max_requests - 1
 	end
 end
 
