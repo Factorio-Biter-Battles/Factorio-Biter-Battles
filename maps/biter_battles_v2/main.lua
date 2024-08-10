c602eb8e071edf780b283d67e00cb7ac15272fa6
developer <88335092+developer-8@users.noreply.github.com>
2024-08-10 23:31:20 +0300

    add comment

--- maps/biter_battles_v2/main.lua
+++ maps/biter_battles_v2/main.lua
@@ -293,6 +293,7 @@ local function on_console_command(event)
 		local to_player = game.get_player(to_player_name)
 		if to_player then
 			do_ping(player.name, to_player, player.name .. " (whisper): " .. rest_of_message)
+			-- to_player_name is case insensitive, so use to_player.name instead
 			global.reply_target[to_player.name] = player.name
 		end
 	elseif cmd == "r" or cmd == "reply" then
