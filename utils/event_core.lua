8e104e704677ceef0356480571767ec14d2bbc55
Cliff Frey <clifffrey@gmail.com>
2024-08-18 15:42:32 -0700

    Revert "Add event priority (#588)"
    This reverts commit 60609ca3e89877ef82ef49b344fe4de1290854ec.

--- utils/event_core.lua
+++ utils/event_core.lua
@@ -6,10 +6,9 @@ local Public = {}
 local init_event_name = -1
 local load_event_name = -2
 
----@type { [EventName]: { handler: fun(), priority: number }[] }
+-- map of event_name to handlers[]
 local event_handlers = {}
-
----@type { [int]: { handler: fun(), priority: number }[] }
+-- map of nth_tick to handlers[]
 local on_nth_tick_event_handlers = {}
 
 --[[ local interface = {
@@ -33,17 +32,21 @@ local function errorHandler(err)
     log(debug.traceback())
 end
 
----@param handlers { handler: fun(...: any), priority: number }[]
----@param event any
-local function call_handlers(handlers, event)
+local call_handlers
+function call_handlers(handlers, event)
     if not handlers then
         return log('Handlers was nil!')
     end
     local handlers_copy = table.deepcopy(handlers)
-    for i = 1, #handlers_copy do
-        local handler = handlers_copy[i]
+    for i = 1, #handlers do
+        local handler = handlers[i]
+        if handler == nil and handlers_copy[i] ~= nil then
+            if table.contains(handlers, handlers_copy[i]) then
+                handler = handlers_copy[i]
+            end
+        end
         if handler ~= nil then
-            xpcall(handler.handler, errorHandler, event)
+            xpcall(handler, errorHandler, event)
         else
             log('nil handler')
         end
@@ -85,102 +88,62 @@ local function on_nth_tick_event(event)
     call_handlers(handlers, event)
 end
 
----A binary search that sorts in reversed order (highest first, lower last).
----It also doesn't stop when the index is found to ensure that
----handlers with the same priority will be called in order of insertion.
----@param handlers { priority: number }[]
----@param target number
----@return int
-local function priority_binary_search(handlers, target)
-    local lower = 1
-    local upper = #handlers
-
-    while lower <= upper do
-        local i = math.floor((lower + upper) / 2)
-        if target > handlers[i].priority then
-            upper = i - 1
-        else
-            lower = i + 1
-        end
-    end
-
-    return lower
-end
-
----Do not use this function, use Event.add instead as it has safety checks.
----@param event_name EventName
----@param handler fun(event: EventData)
----@param priority number?
-function Public.add(event_name, handler, priority)
-    if not priority then
-        priority = 0
-    end
+--- Do not use this function, use Event.add instead as it has safety checks.
+function Public.add(event_name, handler)
     if event_name == defines.events.on_entity_damaged then
         error('on_entity_damaged is managed outside of the event framework.')
     end
     local handlers = event_handlers[event_name]
     if not handlers then
-        event_handlers[event_name] = {}
-    end
-
-    table.insert(handlers, priority_binary_search(handlers, priority), { handler = handler, priority = priority })
-    if #handlers == 1 then
+        event_handlers[event_name] = { handler }
         script_on_event(event_name, on_event)
+    else
+        table.insert(handlers, handler)
+        if #handlers == 1 then
+            script_on_event(event_name, on_event)
+        end
     end
 end
 
----Do not use this function, use Event.on_init instead as it has safety checks.
----@param handler fun(event: EventData)
----@param priority number?
-function Public.on_init(handler, priority)
-    if not priority then
-        priority = 0
-    end
+--- Do not use this function, use Event.on_init instead as it has safety checks.
+function Public.on_init(handler)
     local handlers = event_handlers[init_event_name]
     if not handlers then
-        event_handlers[init_event_name] = {}
-    end
-
-    table.insert(handlers, priority_binary_search(handlers, priority), { handler = handler, priority = priority })
-    if #handlers == 1 then
+        event_handlers[init_event_name] = { handler }
         script.on_init(on_init)
+    else
+        table.insert(handlers, handler)
+        if #handlers == 1 then
+            script.on_init(on_init)
+        end
     end
 end
 
----Do not use this function, use Event.on_init instead as it has safety checks.
----@param handler fun(event: EventData)
----@param priority number?
-function Public.on_load(handler, priority)
-    if not priority then
-        priority = 0
-    end
+--- Do not use this function, use Event.on_load instead as it has safety checks.
+function Public.on_load(handler)
     local handlers = event_handlers[load_event_name]
     if not handlers then
-        event_handlers[load_event_name] = {}
-    end
-
-    table.insert(handlers, priority_binary_search(handlers, priority), { handler = handler, priority = priority })
-    if #handlers == 1 then
+        event_handlers[load_event_name] = { handler }
         script.on_load(on_load)
+    else
+        table.insert(handlers, handler)
+        if #handlers == 1 then
+            script.on_load(on_load)
+        end
     end
 end
 
----Do not use this function, use Event.on_nth_tick instead as it has safety checks.
----@param tick int
----@param handler fun(event: EventData)
----@param priority number?
-function Public.on_nth_tick(tick, handler, priority)
-    if not priority then
-        priority = 0
-    end
+--- Do not use this function, use Event.on_nth_tick instead as it has safety checks.
+function Public.on_nth_tick(tick, handler)
     local handlers = on_nth_tick_event_handlers[tick]
     if not handlers then
-        on_nth_tick_event_handlers[tick] = { { handler = handler, priority = priority } }
-    end
-
-    table.insert(handlers, priority_binary_search(handlers, priority), { handler = handler, priority = priority })
-    if #handlers == 1 then
+        on_nth_tick_event_handlers[tick] = { handler }
         script_on_nth_tick(tick, on_nth_tick_event)
+    else
+        table.insert(handlers, handler)
+        if #handlers == 1 then
+            script_on_nth_tick(tick, on_nth_tick_event)
+        end
     end
 end
 
