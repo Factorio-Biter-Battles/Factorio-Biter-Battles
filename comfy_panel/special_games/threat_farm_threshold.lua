5b5ba5e9790d0a5dbb7270bd49f36a1b75173812
Arktischer <arktischer@gmx.de>
2024-09-05 06:08:54 +0200

    added checkbox to hide chat threat farm special chat message to enable admin abuse, implemented some suggestions from cliff, code formatted

--- comfy_panel/special_games/threat_farm_threshold.lua
+++ comfy_panel/special_games/threat_farm_threshold.lua
@@ -1,27 +1,52 @@
-local color_presets = require "utils.color_presets"
+local color_presets = require('utils.color_presets')
 
 local Public = {
-   name = {type = "label", caption = "threat threshold", tooltip= "threat cant fall below threshold, fraction of excess threat is send to enemy"},
-   config = {
-    [1] = {name = "label1", type = "label", caption = "minimum threat amount"},
-    [2] = {name = "threat_farm_threshold", type = "textfield", width = 40, text=0},
-    [3] = {name = "label2", type = "label", caption="fraction of excess threat, send to enemy"},
-    [4] = {name = "threat_farm_send_fraction", type= "textfield", width = 40, text=0.5},
-   },
-   button = {name = "apply", type= "button", caption = "Apply"},
-   generate = function (config, player)
-        if(not tonumber(config["threat_farm_threshold"].text)or not tonumber(config["threat_farm_send_fraction"].text)) then
-            game.print("invalid configuration, only numbers allowed",color_presets.warning)
-            return
+    name = {
+        type = 'label',
+        caption = 'threat threshold',
+        tooltip = 'threat cant fall below threshold, fraction of excess threat is send to enemy',
+    },
+    config = {
+        [1] = { name = 'l1', type = 'label', caption = 'minimum threat amount' },
+        [2] = {
+            name = 'threat_threshold',
+            type = 'textfield',
+            numeric = true,
+            allow_negative = true,
+            width = 40,
+            text = 0,
+        },
+        [3] = { name = 'l2', type = 'label', caption = 'fraction of excess threat, send to enemy' },
+        [4] = {
+            name = 'excess_threat_send_fraction',
+            type = 'textfield',
+            numeric = true,
+            allow_decimal = true,
+            allow_negative = true,
+            width = 40,
+            text = 0.5,
+        },
+        [5] = { name = 'l3', type = 'label', caption = 'hide message' },
+        [6] = { name = 'hide message', type = 'checkbox', state = false },
+    },
+    button = { name = 'apply', type = 'button', caption = 'Apply' },
+    generate = function(config, player)
+        local variables = {
+            threat_threshold = config['threat_threshold'].text,
+            excess_threat_send_fraction = config['excess_threat_send_fraction'].text,
+        }
+
+        global.active_special_games['threat_farm_threshold'] = true
+        global.special_games_variables['threat_farm_threshold'] = variables
+        if not config['hide message'].state then
+            game.print(
+                'Threat threshold enabled! Excess threat below '
+                    .. variables.threat_threshold
+                    .. ' will be added to opponent with a factor of '
+                    .. variables.excess_threat_send_fraction,
+                color_presets.warning
+            )
         end
-
-        global.active_special_games["threat_farm_threshold"] = true
-        global.special_games_variables["threat_farm_threshold"] = tonumber(config["threat_farm_threshold"].text)
-        global.special_games_variables["threat_farm_send_fraction"] = tonumber(config["threat_farm_send_fraction"].text)
-
-        
-        
-        game.print("excess threat below " .. global.special_games_variables["threat_farm_threshold"] .. " will be added to opponent with a factor of " .. global.special_games_variables["threat_farm_send_fraction"], color_presets.warning)
     end,
 }
 return Public
