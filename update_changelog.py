d068ae64d7ac6bb480cef9a13f82a70c4a965325
cogito123 <59515242+cogito123@users.noreply.github.com>
2025-01-18 13:19:29 +0100

    update_changelog: adapt to latest format
    In order to save time constructing in-game changelog we have to rotate
    between json and lua code internally.

--- update_changelog.py
+++ update_changelog.py
@@ -2,8 +2,12 @@
 """
 
 import json
+import os
 import sys
 import requests
+from luaparser import ast
+from luaparser.astnodes import (Table, Chunk, Block, Field, Name,
+                                Return, String, Number, StringDelimiter)
 
 GIT_NAME_MAPPING = {
     "Ragnarok77-factorio": "Ragnarok77",
@@ -31,9 +35,10 @@ def parse_entry(pull_req):
     date = pull_req["merged_at"].split("T")[0]
     name = get_name(pull_req['user']['login'])
     return {
+        'number': pull_req['number'],
         'date': sanitize_string(date),
-        'title': sanitize_string(pull_req['title']),
-        'login': sanitize_string(name)
+        'comment': sanitize_string(pull_req['title']),
+        'author': sanitize_string(name)
     }
 
 def is_valid_entry(pull_req):
@@ -57,7 +62,7 @@ def fetch_pr_page(auth, page):
 
     return requests.get(GH_URL, auth=auth, params=params).json()
 
-def collect_entries():
+def gh_collect_entries():
     """ Queries GH pull requests and creates entries out of them for the changelog
     """
     merged_pull_requests = []
@@ -82,6 +87,117 @@ def collect_entries():
 
     return entries
 
+def lua_find_table(tree):
+    """ Walks through AST to find first Table node.
+    """
+
+    for node in ast.walk(tree):
+        if isinstance(node, Table):
+            break
+    return node
+
+def lua_extract_entry(field):
+    """ Extracts single entry from Table node entry.
+    """
+
+    entry = field.value.fields
+    return {
+        'number': entry[0].value.n,
+        'date': entry[1].value.s,
+        'author': entry[2].value.s,
+        'comment': entry[3].value.s
+    }
+
+class SuppressStdOutput:
+    """ Suppresses standard output of noisy modules
+    """
+
+    def __init__(self):
+        self.prev = None
+
+    def __enter__(self):
+        self.prev = sys.stdout
+        sys.stdout = open(os.devnull, 'w', encoding='utf-8')
+
+    def __exit__(self, _, __, ___):
+        sys.stdout.close()
+        sys.stdout = self.prev
+
+def lua_collect_entries():
+    """ Go through existing change log and parse entries back to json.
+    """
+
+    with open("maps/biter_battles_v2/changes.lua", 'r', encoding='utf-8') as lua:
+        src = lua.read()
+    with SuppressStdOutput():
+        tree = ast.parse(src)
+    root = lua_find_table(tree)
+    entries = []
+    for field in root.fields:
+        entries.append(lua_extract_entry(field))
+    return entries
+
+def trim_whitespace_unordered_list(string):
+    """ Trims whitespaces of a string that contains unordered list entries
+    to proper offset.
+    e.g.:
+    - position 1
+    - position 2
+    ...
+    """
+
+    new = []
+    for line in string.splitlines():
+        candidate = line.lstrip()
+        if candidate and candidate[0] == '-':
+            line = '  ' + candidate
+        new += line + '\n'
+    return ''.join(new)
+
+def lua_dump_entries(entries):
+    """ Convert json entries back into lua entries.
+    """
+
+    fields = []
+    for entry in entries:
+        _fields = [
+            Field(Name('number'), Number(entry['number'])),
+            Field(Name('date'), String(entry['date'], StringDelimiter.SINGLE_QUOTE)),
+            Field(Name('author'), String(entry['author'], StringDelimiter.SINGLE_QUOTE)),
+            Field(Name('comment'), String(entry['comment'], StringDelimiter.DOUBLE_SQUARE)),
+        ]
+        fields.append(Table(_fields))
+
+    exp = Chunk(Block([
+        Return([Table(fields=fields)]),
+    ]))
+
+    src = ast.to_lua_source(exp)
+    src = trim_whitespace_unordered_list(src)
+    with open("maps/biter_battles_v2/changes.lua", 'w', encoding='utf-8') as lua:
+        lua.write(src)
+
+def gh_filter_unique_pulls(entries, pulls):
+    """ Goes through all available PRs and outputs new/fresh ones that shall
+    be added to changelog.
+    """
+
+    threshold = max(entry['number'] for entry in entries)
+    new = []
+    for pull in pulls:
+        if pull['number'] <= threshold:
+            continue
+        new.append(pull)
+
+    return new
+
+def gh_tag_pulls(pulls):
+    """ Tag new entries with PR number for reference
+    """
+
+    for pull in pulls:
+        pull['comment'] = f"(#{pull['number']}) {pull['comment']}"
+
 def main():
     print("Usage of script with usage of GitHub token for more API requests: python scriptName username token")
     print("Usage of script without any token: python scriptName")
@@ -94,23 +210,11 @@ def main():
     else:
         print('Wrong number of arguments (should be 2 or 0) for the script, will use the default connection to the GitHub API without any token')
 
-    entries = collect_entries()
-    with open("maps/biter_battles_v2/changelog_tab.lua", 'r', encoding='utf-8') as log:
-        lines = log.readlines()
-
-    source = []
-    found_first_line = 0
-    for line in lines:
-        if "\tadd_entry(" in line and found_first_line == 0:
-            found_first_line = 1
-            for entry in entries:
-                name = entry['login']
-                source.append("\tadd_entry(\"" + entry['date'] + "\", \"" + name + "\", \"" + entry['title'] + "\")\n")
-        if "\tadd_entry(" not in line:
-            source.append(line)
-
-    with open("maps/biter_battles_v2/changelog_tab.lua", 'w', encoding='utf-8') as output:
-        output.writelines(source)
+    pulls = gh_collect_entries()
+    entries = lua_collect_entries()
+    new = gh_filter_unique_pulls(entries, pulls)
+    gh_tag_pulls(new)
+    lua_dump_entries(new + entries)
 
 if __name__ == '__main__':
     main()
