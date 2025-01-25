54a00564f9e7123b86a3f6f3ae9b3ca2a01a8c73
Data <DaTa-@users.noreply.github.com>
2025-01-25 04:58:29 +0200

    Terrain: optimize mixed tile and biter area border generation by reducing amount of noise calculation
    Performance:
    all avg -25%, median -37%
    river avg -15%, median
    ordinary avg -33%, median -41%
    biter area border avg -10%, median -9%

--- utils/multi_octave_noise.lua
+++ utils/multi_octave_noise.lua
@@ -15,4 +15,28 @@ return {
         end
         return noise
     end,
+
+    --- Calculates noise value for given octaves, short circuits if further computations won't exceed `lower_bound` value
+    ---
+    --- This function expects octaves ordered by positive amplitudes in descending order
+    ---@param octaves [{amp: number, freq: number}]
+    ---@param amplitude_sum number # precalculated sum of amplitudes of all layers
+    ---@param pos {x: number, y: number}
+    ---@param seed number
+    ---@param offset number offset of the seed between each octave. Providing low value will result at interference near origin point
+    ---@param lower_bound number
+    ---@return number? # of the range [-1; 1] * (sum of all amplitudes), if value exceeds cutoff
+    get_lower_bounded = function(octaves, amplitude_sum, pos, seed, offset, lower_bound)
+        local noise = 0.0
+        local amplitude_left = amplitude_sum
+        for _, octave in ipairs(octaves) do
+            noise = noise + simplex_noise(pos.x * octave.freq, pos.y * octave.freq, seed) * octave.amp
+            amplitude_left = amplitude_left - octave.amp
+            if noise + amplitude_left <= lower_bound then
+                return nil
+            end
+            seed = seed + offset
+        end
+        return noise
+    end,
 }
