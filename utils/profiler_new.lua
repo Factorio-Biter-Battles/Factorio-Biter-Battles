62886b935043e8445f5a8ba8184128eadbb21238
TheBigZet <92057893+TheBigZet@users.noreply.github.com>
2025-02-06 22:45:28 +0100

    reduced table recreation in tick_durations

--- utils/profiler_new.lua
+++ utils/profiler_new.lua
@@ -8,67 +8,49 @@ storage.profiler_new = {
 --- starting at index 2, as first entry is a constant empty string
 local index = 2
 ---preallocate array of log entries
+-- stylua: ignore
 local tick_durations = {
     '',
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-}
----preallocate array of LuaProfilers
----@type 0[]|LuaProfiler[]
-local profilers = {
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
+    {'',0,'\t',helpers.create_profiler(true), '\n'},
 }
 
 --- Measure duration of each tick update.
 --- Dumping on every 19th tick to minimize number of write_file() calls
 local measure_tick_duration = Token.register(function(event)
-    ---stop the profiler started in previous tick
-    profilers[index].stop()
-    ---log its data
-    tick_durations[index] = { '', event.tick - 1, '\t', profilers[index], '\n' }
+    ---stop the profiler started in previous tick and log its data
+    tick_durations[index][2] = event.tick - 1
+    tick_durations[index][4].stop()
     index = index + 1
     ---dump data when LocalisedString limit size is reached
     if index == 21 then
-        helpers.write_file('profiler/cumulative/total_tick_duration.txt', tick_durations, true, storage.profiler_new.player)
+        helpers.write_file(
+        'profiler/cumulative/total_tick_duration.txt',
+            tick_durations,
+            true,
+            storage.profiler_new.player
+        )
         index = 2
     end
     ---start timer for this tick
-    profilers[index].reset()
+    tick_durations[index][4].reset()
 end)
 
 ---Start the profiler
@@ -85,10 +67,8 @@ local function profiler_start(cmd)
         return
     end
     storage.profiler_new = { enabled = true, player = cmd.player_index }
-    for i = 2, 20, 1 do
-        profilers[i] = helpers.create_profiler(true)
-    end
-    profilers[2].restart()
+
+    tick_durations[2][4].restart()
     Event.add_removable(defines.events.on_tick, measure_tick_duration)
     game.print('====Profiler started====\n')
 end
@@ -106,33 +86,14 @@ local function profiler_stop(cmd)
         player.print("Profiler isn't running")
         return
     end
-    for i = index, 20, 1 do
-        tick_durations[i] = { '', '' }
+    tick_durations[index][2] = cmd.tick-1
+    tick_durations[index][4].stop()
+    for i = 2,index, 1 do
+        helpers.write_file('profiler/cumulative/total_tick_duration.txt', tick_durations[i], true, storage.profiler_new.player)
     end
-    helpers.write_file('profiler/total_tick_duration.txt', tick_durations, true, storage.profiler_new.player)
     index = 2
     storage.profiler_new.enabled = false
-    profilers = {
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-    }
+
     Event.remove_removable(defines.events.on_tick, measure_tick_duration)
     game.print('====Profiler stopped====\n Check logs in script-output/profiler \n')
 end
