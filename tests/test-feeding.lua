1aa546387d138ae8c9f769e24609642a498ad5e4
cogito123 <59515242+cogito123@users.noreply.github.com>
2025-03-13 20:24:03 +0100

    change balance of flasks according to poll
    buff blue +4%
    nerf purple -2%
    buff yellow +2%

--- tests/test-feeding.lua
+++ tests/test-feeding.lua
@@ -42,7 +42,7 @@ function test_feed_effects_3()
     local num_flasks = 4500
     local flask_food_value = Tables.food_values['utility-science-pack'].value * difficulty / 100
     local calc = Functions.calc_feed_effects(evo, flask_food_value, num_flasks, current_player_count, max_reanim_thresh)
-    lunatest.assert_equal('evo_increase: 0.671 threat: 25218', effects_str(calc))
+    lunatest.assert_equal('evo_increase: 0.675 threat: 25614', effects_str(calc))
 end
 
 function test_feed_effects_4()
