56a8607f8cf8ecb05065292f3a96dd2ad6203898
TheBigZet <92057893+TheBigZet@users.noreply.github.com>
2025-03-30 06:47:03 +0200

    Clear corpses tweaks (#708)
    * Moved /clear-corpses command to commands dir;
    Added default radius to /clear-corpses;
    Tweaked naming in Functions.clear_corpses;
    Replaced errors with warning/infos in /clear_corpses
    
    * attempt at fixing styling
    
    * removed untrusted_max_radius localization
    
    * added safe_wrap_cmd

---
+++ commands/clear_corpses.lua
@@ -0,0 +1,43 @@
+local Functions_clear_corpses = require('maps.biter_battles_v2.functions').clear_corpses
+local Utils_safe_wrap_cmd = require('utils.utils').safe_wrap_cmd
+local Session = require('utils.datastore.session_data')
+local Color_fail = require('utils.color_presets').fail
+local Color_warning = require('utils.color_presets').warning
+
+local trusted_max_radius = 500
+
+local function clear_corpses(cmd)
+    local player = game.get_player(cmd.player_index)
+    local trusted = Session.get_trusted_table()
+    local param = tonumber(cmd.parameter) or storage.default_clear_corpses_radius
+
+    if not player or not player.valid then
+        return
+    end
+
+    if not trusted[player.name] and not player.admin and param > storage.default_clear_corpses_radius then
+        player.print(
+            '[INFO] Replaced radius with max allowable value for untrusted players: '
+                .. storage.default_clear_corpses_radius,
+            { color = Color_warning }
+        )
+        param = storage.default_clear_corpses_radius
+    end
+    if param < 0 then
+        player.print('[ERROR] Value must be positive.', { color = Color_fail })
+        return
+    end
+    if param > 500 then
+        player.print(
+            '[INFO] Replaced radius with max allowable value:  ' .. trusted_max_radius,
+            { color = Color_warning }
+        )
+        param = trusted_max_radius
+    end
+
+    Functions_clear_corpses(player, param)
+end
+
+commands.add_command('clear-corpses', 'Clears all the biter corpses..', function(cmd)
+    Utils_safe_wrap_cmd(cmd, clear_corpses, cmd)
+end)
