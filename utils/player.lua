1c3d9f9c5eb4eb9256e0fe6fc2761b7124a36cb7
TheBigZet <92057893+TheBigZet@users.noreply.github.com>
2025-04-06 11:32:38 +0200

    reduced use of table.insert and table recreation

--- utils/player.lua
+++ utils/player.lua
@@ -1,4 +1,4 @@
-local table_insert = table.insert
+local malloc = require('maps.biter_battles_v2.pool').malloc
 
 local Public = {}
 
@@ -19,18 +19,17 @@ end
 ---@param player_list LuaPlayer[]
 ---@return string[]
 function Public.get_colored_player_list(player_list)
-    local players_with_colors = {}
+    local players_with_colors = malloc(#player_list)
+    local i = 1
     for _, player in pairs(player_list) do
-        table_insert(
-            players_with_colors,
-            string.format(
-                '[color=%.2f,%.2f,%.2f]%s[/color]',
-                player.color.r * 0.6 + 0.4,
-                player.color.g * 0.6 + 0.4,
-                player.color.b * 0.6 + 0.4,
-                player.name
-            )
+        players_with_colors[i] = string.format(
+            '[color=%.2f,%.2f,%.2f]%s[/color]',
+            player.color.r * 0.6 + 0.4,
+            player.color.g * 0.6 + 0.4,
+            player.color.b * 0.6 + 0.4,
+            player.name
         )
+        i = i + 1
     end
 
     return players_with_colors
@@ -39,28 +38,29 @@ end
 ---@param player_list LuaPlayer[]
 ---@return string[]
 function Public.get_sorted_colored_player_list(player_list)
-    local players_with_sort_keys = {}
-    for _, p in pairs(player_list) do
-        table_insert(players_with_sort_keys, { player = p, sort_key = string.lower(p.name) })
+    local players_with_sort_keys = malloc(#player_list)
+    local i = 1
+    for _, player in pairs(player_list) do
+        players_with_sort_keys[i] = { player, string.lower(player.name) }
+        i = i + 1
     end
     table.sort(players_with_sort_keys, function(a, b)
-        return a.sort_key < b.sort_key
+        return a[2] < b[2]
     end)
 
-    local sorted_player_list = {}
-    for _, pair in ipairs(players_with_sort_keys) do
-        table_insert(sorted_player_list, pair.player)
+    for i = 1, #player_list do
+        players_with_sort_keys[i][2] = nil
     end
 
-    return Public.get_colored_player_list(sorted_player_list)
+    return Public.get_colored_player_list(players_with_sort_keys)
 end
 
 ---@param names string[]
 ---@return LuaPlayer[]
 function Public.get_lua_players_from_player_names(names)
-    local players = {}
-    for _, name in pairs(names) do
-        table_insert(players, get_or_create_dummy_player(name))
+    local players = malloc(#names)
+    for i = 1, #names do
+        players[i] = get_or_create_dummy_player(names[i])
     end
     return players
 end
