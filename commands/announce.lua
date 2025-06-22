e8659c3d1918337be53856cb555f84304f4e6ef4
ness056 <65729201+ness056@users.noreply.github.com>
2025-06-22 16:47:59 +0200

    Add /announce-append command

--- commands/announce.lua
+++ commands/announce.lua
@@ -34,40 +34,59 @@ Public.announce_if_any = function()
     announcement.entity = e
 end
 
+---@param player_index number?
+---@return boolean, LuaPlayer?
+local function check_player_permission(player_index)
+    if not player_index then
+        return false
+    end
+
+    ---@type LuaPlayer?
+    local player = game.get_player(player_index)
+    if not player or not player.valid then
+        return false
+    end
+
+    if not player.admin then
+        player.print('This command can only be used by admins')
+        return false
+    end
+
+    return true, player
+end
+
+---@param player LuaPlayer?
+---@return boolean
+local function try_destroy_bubble(player)
+    local bubble = storage.announcement.entity
+    if bubble and bubble.valid then
+        local removed = bubble.destroy()
+        if not removed then
+            if player and player.valid then
+                player.print('unknown problem occured when removing the announcement')
+                player.print('save the game for later analysis')
+            end
+            return false
+        end
+    end
+
+    return true
+end
+
 ---Implements /announce command.
 ---If no parameter is set, it will clear existing announcement if any.
 ---If parameter is set, it will create new announcement or overwrite existing one.
 ---The announcement is supposed to persist across map resets.
 ---@param cmd CustomCommandData
 local function announce(cmd)
-    ---@type number?
-    local index = cmd.player_index
-    if not index then
+    local allowed, player = check_player_permission(cmd.player_index)
+    if not allowed then
         return
     end
-
-    ---@type LuaPlayer?
-    local player = game.get_player(index)
-    if not player or not player.valid then
-        return
-    end
-
-    if not player.admin then
-        player.print('This command can only be used by admins')
-        return
-    end
+    ---@cast player -nil
 
     -- In any case, we're going to clear existing announcement if any.
-    local removed = false
-    local bubble = storage.announcement.entity
-    if bubble and bubble.valid then
-        removed = bubble.destroy()
-        if not removed then
-            player.print('unknown problem occured when removing the announcement')
-            player.print('save the game for later analysis')
-            return
-        end
-    end
+    local removed = try_destroy_bubble(player)
 
     storage.announcement = {}
     local text = cmd.parameter
@@ -88,6 +107,40 @@ end
 
 commands.add_command('announce', 'Creates a text at spectator island', function(cmd)
     Utils.safe_wrap_cmd(cmd, announce, cmd)
+end)
+
+---Implements /announce-append command.
+---If no parameter is set, does nothing.
+---If parameter is set, it will create new announcement if none exist or append existing one.
+---The announcement is supposed to persist across map resets.
+---@param cmd CustomCommandData
+local function announce_append(cmd)
+    local allowed, player = check_player_permission(cmd.player_index)
+    if not allowed then
+        return
+    end
+    ---@cast player -nil
+
+    local text = cmd.parameter
+    if not text then return end
+
+    -- In any case, we're going to clear existing announcement if any.
+    try_destroy_bubble(player)
+
+    text = text:gsub("\\n", "\n")
+
+    if storage.announcement.text then
+        storage.announcement.text = storage.announcement.text .. '[font=var]' .. text .. '[/font]'
+        Core.print_admins(player.name .. ' appended an announcement')
+    else
+        storage.announcement.text = '[font=var]' .. text .. '[/font]'
+        Core.print_admins(player.name .. ' made an announcement')
+    end
+    Public.announce_if_any()
+end
+
+commands.add_command('announce-append', 'Appends to the text at spectator island', function(cmd)
+    Utils.safe_wrap_cmd(cmd, announce_append, cmd)
 end)
 
 return Public
