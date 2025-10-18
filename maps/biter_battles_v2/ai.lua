67380f3530c3b1100622a683936fa4b4958e5dc4
cogito123 <59515242+cogito123@users.noreply.github.com>
2025-10-18 10:34:55 +0200

    ai: raise_evo: fix wrong timer used in difficulty ramp up
    The game.ticks_played measures ticks since the scenario start or last
    map reset. If nobody is playing and so round has not started yet, the
    difficulty ramp up will still occur. Using match time prevents this
    problem. Originally found by Max1000.

--- maps/biter_battles_v2/ai.lua
+++ maps/biter_battles_v2/ai.lua
@@ -390,7 +390,7 @@ Public.raise_evo = function()
         return
     end
     if storage.difficulty_vote_index and 1 <= storage.difficulty_vote_index and 4 >= storage.difficulty_vote_index then
-        local matchTimeInMinutes = game.ticks_played / 3600
+        local matchTimeInMinutes = Functions.get_ticks_since_game_start() / 3600
         storage.difficulty_vote_value = ((matchTimeInMinutes / 470) ^ 3.7)
             + Tables.difficulties[storage.difficulty_vote_index].value
     end
