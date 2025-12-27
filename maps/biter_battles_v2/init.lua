aeab2da5bc26f70b82381882ad33bf6657324c3c
zer0sun <marcuzfelicianus@gmail.com>
2025-12-27 11:14:03 -0300

    updated after some cogito review

--- maps/biter_battles_v2/init.lua
+++ maps/biter_battles_v2/init.lua
@@ -419,6 +419,12 @@ function Public.tables()
             ping_header.destroy()
         end
     end
+    -- Threat nerf from non positive biters 
+    -- inserted ids of biters are from positive threat generation
+    -- readed when enemy is killed
+    -- cleared when biter this this id dies
+    ---@type { [integer]: true }
+    storage.biters_from_positive_threat = {}
 end
 
 function Public.forces()
