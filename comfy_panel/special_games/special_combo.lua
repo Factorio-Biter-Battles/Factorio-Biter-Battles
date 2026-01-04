6931df3f59986cc79073457127e32ce2012fd8cd
PlayerNoon <120744927+PlayerNoon@users.noreply.github.com>
2026-01-04 23:04:32 +0000

    Fix StyLua

--- comfy_panel/special_games/special_combo.lua
+++ comfy_panel/special_games/special_combo.lua
@@ -1,9 +1,9 @@
 local Color = require('utils.color_presets')
 
 local function generate_fordeka()
-    game.print("Generated special: fordeka",{ color = Color.warning })
-    for k , force in pairs({'north', 'south'}) do
-        game.forces[force].manual_mining_speed_modifier=4000
+    game.print('Generated special: fordeka', { color = Color.warning })
+    for k, force in pairs({ 'north', 'south' }) do
+        game.forces[force].manual_mining_speed_modifier = 4000
         game.forces[force].technologies['construction-robotics'].researched = true
         game.forces[force].technologies['worker-robots-speed-1'].researched = true
         game.forces[force].technologies['worker-robots-speed-2'].researched = true
@@ -12,23 +12,23 @@ local function generate_fordeka()
         game.forces[force].technologies['worker-robots-speed-5'].researched = true
         game.forces[force].technologies['worker-robots-speed-6'].level = 15
     end
-    local eq = {     
-        'power-armor-mk2',     
-        'personal-roboport-mk2-equipment',     
+    local eq = {
+        'power-armor-mk2',
+        'personal-roboport-mk2-equipment',
         'battery-mk2-equipment',
-        'exoskeleton-equipment',     
-        'construction-robot',     
+        'exoskeleton-equipment',
+        'construction-robot',
         'fission-reactor-equipment',
         'electric-mining-drill',
     }
-    local gap = 0 
-    local operable = false  
-    local surface = game.surfaces[storage.bb_surface_name] 
-    local position_0 = { x = 0, y = -43 }  
+    local gap = 0
+    local operable = false
+    local surface = game.surfaces[storage.bb_surface_name]
+    local position_0 = { x = 0, y = -43 }
     local objects = surface.find_entities_filtered({ name = 'infinity-chest' })
     for _, object in pairs(objects) do
         object.destroy()
-    end  
+    end
     local k = gap + 1
     for i, v in ipairs(eq) do
         local chest = surface.create_entity({
@@ -56,32 +56,30 @@ local function generate_fordeka()
     end
 end
 
-
-
 local function generate_carl3()
-    game.print("Generated special: carl",{ color = Color.warning })
-        for k , force in pairs({'north', 'south'}) do
-        game.forces[force].manual_mining_speed_modifier=4000
+    game.print('Generated special: carl', { color = Color.warning })
+    for k, force in pairs({ 'north', 'south' }) do
+        game.forces[force].manual_mining_speed_modifier = 4000
         game.forces[force].technologies['construction-robotics'].researched = true
         game.forces[force].technologies['worker-robots-speed-1'].researched = true
         game.forces[force].technologies['worker-robots-speed-2'].researched = true
         game.forces[force].technologies['worker-robots-speed-3'].researched = true
         game.forces[force].technologies['worker-robots-speed-4'].researched = true
     end
-    local eq = {     
+    local eq = {
         'modular-armor',
         'battery-mk2-equipment',
-        'exoskeleton-equipment', 
+        'exoskeleton-equipment',
         'solar-panel-equipment',
     }
-    local gap = 0 
-    local operable = false  
-    local surface = game.surfaces[storage.bb_surface_name] 
-    local position_0 = { x = 0, y = -43 }  
+    local gap = 0
+    local operable = false
+    local surface = game.surfaces[storage.bb_surface_name]
+    local position_0 = { x = 0, y = -43 }
     local objects = surface.find_entities_filtered({ name = 'infinity-chest' })
     for _, object in pairs(objects) do
         object.destroy()
-    end  
+    end
     local k = gap + 1
     for i, v in ipairs(eq) do
         local chest = surface.create_entity({
@@ -109,14 +107,11 @@ local function generate_carl3()
     end
 end
 
-
-
-
 local Public = {
     name = {
-        type = 'label', 
-        caption = 'special combo', 
-        tooltip = 'use pre-defined settings'
+        type = 'label',
+        caption = 'special combo',
+        tooltip = 'use pre-defined settings',
     },
     config = {
         [1] = {
@@ -125,8 +120,8 @@ local Public = {
             items = { 'fordeka', 'carl3' },
         },
     },
-    button = {name = 'apply', type = 'button', caption = 'Apply'},
-    generate = function (config, player)
+    button = { name = 'apply', type = 'button', caption = 'Apply' },
+    generate = function(config, player)
         if tonumber(config['names'].selected_index) == 1 then
             generate_fordeka()
         end
@@ -134,9 +129,6 @@ local Public = {
             generate_carl3()
         end
     end,
-    
 }
 
-
 return Public
-
